d3f03c52a7baf0ec686d5879cad800a3ffc9a7a1
kevgliss <kevgliss@gmail.com>
2018-05-10 15:29:59 -0700

    We only care about data on UnMarshaled results. (#20)

--- diffy_api/core.py
+++ diffy_api/core.py
@@ -7,7 +7,7 @@ from diffy_api.schemas import baseline_input_schema, analysis_input_schema
 def async_baseline(kwargs):
     """Wrapper job around our standard baseline task."""
     # we can't pickle our objects for remote works so we pickle the raw request and then load it here.
-    data = baseline_input_schema.load(kwargs)
+    data = baseline_input_schema.load(kwargs).data
     return baseline(**data)
 
 
@@ -15,5 +15,5 @@ def async_baseline(kwargs):
 def async_analysis(kwargs):
     """Wrapper job around our standard analysis task."""
     # we can't pickle our objects for remote works so we pickle the raw request and then load it here.
-    data = analysis_input_schema.load(kwargs)
+    data = analysis_input_schema.load(kwargs).data
     return analysis(**data)
