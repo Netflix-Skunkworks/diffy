f2a8764de4c3fc6ba7f19282f4e30fed3b24b026
Forest Monsen <fmonsen@netflix.com>
2018-07-19 11:35:24 -0700

    Getting config from YAML

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,7 +29,7 @@ def app(request):
     Creates a new Flask application for a test duration.
     Uses application factory `create_app`.
     """
-    _app = create_app(os.path.dirname(os.path.realpath(__file__)) + '/conf.py')
+    _app = create_app(os.path.dirname(os.path.realpath(__file__)) + '/config.yml')
     ctx = _app.app_context()
     ctx.push()
 
